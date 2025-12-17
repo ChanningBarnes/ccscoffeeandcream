ba1a36b5ef87d46a7eb444d41d8be8562c0fed76
ChanningBarnes <channinggbarnes@gmail.com>
2025-12-16 20:51:43 -0500

    updating hero header

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,7 +9,7 @@ function HeroSection() {
         {/* Add the image here, before the text */}
         <img src="/images/cchero.png" alt="Coffee pouring into a cup" className="hero-main-image" />
 
-        <h1>Nostalgia in Drink Form</h1>
+        <h1>Nostalgia You Can Taste</h1>
         <div className="hero-buttons">
           <a href="/events" className="btn-primary">Upcoming Events</a>
         </div>
