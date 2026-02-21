a9a5d7c7f8172d50a16c3a975a956ed58bc237b0
ChanningBarnes <channinggbarnes@gmail.com>
2026-02-21 17:16:54 -0500

    updating events

--- src/data/eventData.js
+++ src/data/eventData.js
@@ -1,20 +1,28 @@
 // src/data/eventData.js
 const events = [
   {
-    id: 0,
+    id: 1,
+    title: 'Troubadour Booksellers Pop-Up',
+    date: 'Saturday, March 14th 2026',
+    time: '11:00AM - 6:00PM',
+    description: 'Join us at Troubadour Booksellers for a delicious coffee while you browse their selection of books!',
+    address: '1721 Sardis Road N, Charlotte, NC 28270'
+  },
+    {
+    id: 2,
     title: 'Stegall Market Pop-Up',
-    date: 'Saturday, February 21st 2026',
+    date: 'Saturday, April 25th 2026',
     time: '10:00AM - 2:00PM',
     description: 'Join us at Stegall Market for a day of delicious coffee with our new seasonal menu!',
     address: '6411 Old Monroe Road, Indian Trail, NC 28079'
   },
   {
-    id: 1,
-    title: 'Troubadour Booksellers Pop-Up',
-    date: 'Saturday, March 14th 2026',
-    time: '10:00AM - 6:00PM',
-    description: 'Join us at Troubadour Booksellers for a delicious coffee while you browse their selection of books!',
-    address: '1721 Sardis Road N, Charlotte, NC 28270'
+    id: 3,
+    title: 'Stegall Market Pop-Up',
+    date: 'Saturday, May 9th 2026',
+    time: '10:00AM - 2:00PM',
+    description: 'Join us at Stegall Market for a day of delicious coffee with our new seasonal menu!',
+    address: '6411 Old Monroe Road, Indian Trail, NC 28079'
   }
 ];
 
