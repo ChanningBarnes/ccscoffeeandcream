bfb451a6e3c9a54e9a6464568677ecb95938e289
ChanningBarnes <channinggbarnes@gmail.com>
2026-08-19 17:36:54 -0400

    update events

--- src/data/eventData.js
+++ src/data/eventData.js
@@ -2,30 +2,22 @@
 const events = [
   {
     id: 1,
-    title: 'Crunch Fitness Matthews',
-    date: '7/29',
-    time: '5:00PM - 8:00PM',
-    description: 'We will be serving coffee and connecting with the community at Crunch Fitness Matthews.',
-    address: '10404 E Independence Blvd, Matthews, NC 28105, United States'
+    title: 'Crunch Fitness Arboretum',
+    date: '8/29',
+    time: '9:00AM - 1:00PM',
+    description: 'Stop by Crunch Fitness Arboretum for fresh coffee and a welcoming community atmosphere!',
+    address: '3413 Pineville-Matthews Rd, Charlotte, NC 28226, United States'
   },
   {
     id: 2,
-    title: 'Stegall Product Market',
-    date: '8/2',
-    time: '11:00AM - 4:00PM',
-    description: 'Join us at Stegall Product Market for a great day of coffee and community!',
-    address: '6411 Old Monroe Rd, Indian Trail, NC 28079, United States'
+    title: 'Crunch Fitness Arboretum',
+    date: '8/31',
+    time: '9:00AM - 1:00PM',
+    description: 'Stop by Crunch Fitness Arboretum for fresh coffee and a welcoming community atmosphere!',
+    address: '3413 Pineville-Matthews Rd, Charlotte, NC 28226, United States'
   },
   {
     id: 3,
-    title: 'Stegall Product Market',
-    date: '9/12',
-    time: '10:00AM - 2:00PM',
-    description: 'Join us at Stegall Product Market for a great day of coffee and community!',
-    address: '6411 Old Monroe Rd, Indian Trail, NC 28079, United States'
-  },
-  {
-    id: 4,
     title: 'Stegall Product Market',
     date: '10/24',
     time: '10:00AM - 2:00PM',
